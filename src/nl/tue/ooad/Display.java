9644fd525d75a37bfc4ed249341355b1e28c63b2
s157106 <s157106@nbwin1590.campus.tue.nl>
2019-12-01 15:40:58 +0100

    For now, only the first audio in the first channel can be played.

--- src/nl/tue/ooad/Display.java
+++ src/nl/tue/ooad/Display.java
@@ -5,39 +5,76 @@
  */
 package nl.tue.ooad;
 
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 import javax.swing.JPanel;
 
 /**
  *
  * @author s132054
  */
-public class Display implements FrameConsumer {
+public class Display implements FrameConsumer, LineListener {
 
-    JPanel displayPanel;
+    private boolean isPlaying;
+    private long clipPosition;
+    private long mediaDuration;
+    private JPanel displayPanel;
+    private Clip streamSource;
+    private FrameProducer producer;
+    
     
     public Display(JPanel displayPanel) {
         this.displayPanel = displayPanel;
-    }
-    
-    void turnOff() {
-        
-    }
-    
-    void turnOn() {
-        
-    }
-    
-    void playFrame() {
-        
+        streamSource = null;
+        mediaDuration = 0;
     }
 
-    void pauseFrame() {
-        
+    
+    public void setProducer(FrameProducer producer){
+        this.producer = producer;
     }
     
     @Override
-    public void consumeStream(StreamFrame stream) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setStreamSource(Clip streamSource) {
+        this.streamSource = streamSource;
+        mediaDuration = streamSource.getMicrosecondLength();
+    }
+
+    public boolean containsStreamSource(){
+        return streamSource != null;
     }
     
+    public void play(){
+        streamSource.setFramePosition(0);
+        streamSource.start();
+    }
+         
+    void playStream() {
+        if (streamSource != null){
+            streamSource.setMicrosecondPosition(clipPosition);
+            streamSource.start();
+            isPlaying = true;
+        }
+    }
+
+    void pauseStream() {
+        if (streamSource != null) {
+            clipPosition = streamSource.getMicrosecondPosition();
+            streamSource.stop();
+            isPlaying = false;            
+        }
+    }
+
+    @Override
+    public void update(LineEvent event) {
+         LineEvent.Type type = event.getType();
+         
+        if (type == LineEvent.Type.START) {
+            System.out.println("Playback started.");
+             
+        } else if (type == LineEvent.Type.STOP) {
+            System.out.println("Playback completed.");
+        }
+    }
 }
--- src/nl/tue/ooad/EntertainmentSystem.java
+++ src/nl/tue/ooad/EntertainmentSystem.java
@@ -28,6 +28,7 @@ public class EntertainmentSystem extends javax.swing.JFrame {
         display = new Display(displayPanel);
         
         new Thread(tuner).start();
+        
     }
 
     /**
@@ -40,8 +41,7 @@ public class EntertainmentSystem extends javax.swing.JFrame {
     private void initComponents() {
 
         displayPanel = new javax.swing.JPanel();
-        onButton = new javax.swing.JButton();
-        offButton = new javax.swing.JButton();
+        playButton = new javax.swing.JButton();
         tunerPanel = new javax.swing.JPanel();
         channelSelection = new javax.swing.JComboBox<>();
         channelSelectLabel = new javax.swing.JLabel();
@@ -57,34 +57,28 @@ public class EntertainmentSystem extends javax.swing.JFrame {
 
         displayPanel.setBackground(new java.awt.Color(255, 102, 102));
 
-        onButton.setText("ON");
-        onButton.addActionListener(new java.awt.event.ActionListener() {
+        playButton.setText("Play");
+        playButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                onButtonActionPerformed(evt);
+                playButtonActionPerformed(evt);
             }
         });
-
-        offButton.setText("OFF");
 
         javax.swing.GroupLayout displayPanelLayout = new javax.swing.GroupLayout(displayPanel);
         displayPanel.setLayout(displayPanelLayout);
         displayPanelLayout.setHorizontalGroup(
             displayPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(displayPanelLayout.createSequentialGroup()
-                .addGap(26, 26, 26)
-                .addComponent(onButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(offButton)
+                .addGap(27, 27, 27)
+                .addComponent(playButton)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         displayPanelLayout.setVerticalGroup(
             displayPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(displayPanelLayout.createSequentialGroup()
-                .addGap(33, 33, 33)
-                .addGroup(displayPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(onButton)
-                    .addComponent(offButton))
-                .addContainerGap(33, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(playButton)
+                .addContainerGap(56, Short.MAX_VALUE))
         );
 
         tunerPanel.setBackground(new java.awt.Color(153, 255, 153));
@@ -194,9 +188,16 @@ public class EntertainmentSystem extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void onButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onButtonActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_onButtonActionPerformed
+    private void playButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_playButtonActionPerformed
+        
+        if (playButton.getText().equals("Play")){
+            play();
+            playButton.setText("Pause");
+        } else {
+            pause();
+            playButton.setText("Play");
+        }       
+    }//GEN-LAST:event_playButtonActionPerformed
 
     private void programGuideToggleBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_programGuideToggleBtnActionPerformed
         if(programGuideToggleBtn.isSelected()) {
@@ -210,7 +211,7 @@ public class EntertainmentSystem extends javax.swing.JFrame {
     }//GEN-LAST:event_programGuideToggleBtnActionPerformed
 
     private void channelSelectionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_channelSelectionActionPerformed
-        // TODO add your handling code here:
+        // TODO add your handling code here:  
     }//GEN-LAST:event_channelSelectionActionPerformed
 
     /**
@@ -254,8 +255,7 @@ public class EntertainmentSystem extends javax.swing.JFrame {
     private javax.swing.JPanel displayPanel;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JButton offButton;
-    private javax.swing.JButton onButton;
+    private javax.swing.JButton playButton;
     private javax.swing.JPanel programGuidePanel;
     private javax.swing.JToggleButton programGuideToggleBtn;
     private javax.swing.JList<String> programJList;
@@ -265,11 +265,16 @@ public class EntertainmentSystem extends javax.swing.JFrame {
     // End of variables declaration//GEN-END:variables
 
     void play() {
-        
+        if (!display.containsStreamSource()){
+            display.setStreamSource(tuner.getStreamSource());
+            display.playStream();
+        } else {
+            display.playStream();        
+        }
     }
     
     void pause() {
-        
+        display.pauseStream();
     }
     
     void stop() {
